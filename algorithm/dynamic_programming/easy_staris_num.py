f88b88a14f52f9a4c69e51e35c34b1fb899d2ed7
jjy <galid1@naver.com>
2019-04-14 21:08:28 +0900

    19.04.014
    flood fill, dynamic_programming

---
+++ algorithm/dynamic_programming/easy_staris_num.py
@@ -0,0 +1,39 @@
+# 백준 10844 쉬운 계단 수
+
+import sys
+
+def easy_stairs_num(n):
+    # n = 1일때
+    for i in range(1,10):
+        d[1][i] = 1
+    # n = 2일때 , n = 3일때 맨마지막 수가 1인경우 0,2 가 올수있는게 다르므로 따로 처리
+    for j in range(0,10):
+        if j == 0:
+            d[2][j] = d[1][1]
+            continue
+        if j == 1:
+            d[2][j] = d[1][2]
+        if j == 9:
+            d[2][j] = d[1][8]
+            continue
+        d[2][j] = d[1][j - 1] + d[1][j + 1]
+
+    # n >= 3 맨 마지막수가 1일때 0, 2 가 올 수 있음
+    for k in range(3, n+1):
+        for h in range(0, 10):
+            if h == 0:
+                d[k][h] = (d[k - 1][1])%1000000000
+                continue
+            if h == 9:
+                d[k][h] = (d[k - 1][8])%1000000000
+                continue
+            d[k][h] = (d[k - 1][h - 1] + d[k - 1][h + 1])%1000000000
+
+    result = 0
+    for g in range(0,10):
+        result += d[n][g]
+    print((result)%1000000000)
+
+d = [[0 for i in range(10)] for i in range(101)]
+n = int(sys.stdin.readline())
+easy_stairs_num(n)
---
+++ algorithm/flood_fill/complex_numbering.py
@@ -0,0 +1,74 @@
+# 백준 2667번 단지번호 붙이기
+
+import sys
+import collections
+
+class Node:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def search(start, matrix):
+    count = 0
+    stack = collections.deque()
+
+    stack.append(start)
+
+    while stack:
+        cur = stack.pop()
+        matrix[cur.x][cur.y] = 0
+        count += 1
+
+        #상
+        if cur.x - 1 >= 0 :
+            if matrix[cur.x - 1][cur.y] == 1:
+                stack.append(Node(cur.x - 1, cur.y))
+                matrix[cur.x - 1][cur.y] = 0
+        #하
+        if cur.x + 1 < len(matrix) :
+            if matrix[cur.x + 1][cur.y] == 1:
+                stack.append(Node(cur.x + 1, cur.y))
+                matrix[cur.x + 1][cur.y] = 0
+        #좌
+        if cur.y - 1 >= 0 :
+            if matrix[cur.x][cur.y - 1] == 1:
+                stack.append(Node(cur.x, cur.y - 1))
+                matrix[cur.x][cur.y - 1] = 0
+        #우
+        if cur.y + 1 < len(matrix[0]) :
+            if matrix[cur.x][cur.y + 1] == 1:
+                stack.append(Node(cur.x, cur.y + 1))
+                matrix[cur.x][cur.y + 1] = 0
+
+    return count
+
+def complex_numbering(matrix):
+    q = collections.deque()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 1:
+                q.append(Node(i,j))
+
+    result = []
+    while q :
+        cur = q.popleft()
+
+        if matrix[cur.x][cur.y] == 0:
+            continue
+
+        result.append(search(cur, matrix))
+
+    print(len(result))
+    result.sort()
+    for k in result:
+        print(k)
+
+
+matrix = []
+n = int(sys.stdin.readline())
+for i in range(n):
+    matrix.append(list(map(int,list(sys.stdin.readline().rstrip()))))
+
+complex_numbering(matrix)
+
