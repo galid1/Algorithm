00a31a196b7034eaf7ddc9c1cc2014a7fea417ee
jjy <galid1@naver.com>
2019-04-18 16:25:25 +0900

    19.04.17
    Bruteforce (2039), dynamic programming(11053)

---
+++ 2.algorithm/brute_force/seven_dwarfs.py
@@ -0,0 +1,24 @@
+# 백준 2309번 일곱 난쟁이
+
+import sys
+
+def seven_dwarfs(dwarfs):
+    for i in range(0,8):
+        for j in range(i+1, 9):
+            height_sum = []
+            for k in range(0,9):
+                if k == i or k == j:
+                    continue
+                height_sum.append(dwarfs[k])
+
+            if sum(height_sum) == 100:
+                height_sum.sort()
+                for g in height_sum:
+                    print(g)
+                return
+
+
+dwarfs = []
+for i in range(9):
+    dwarfs.append(int(sys.stdin.readline()))
+seven_dwarfs(dwarfs)
