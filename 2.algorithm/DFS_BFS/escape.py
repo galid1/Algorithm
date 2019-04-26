b2d75e02232823358e1cddd4223e91c7789b9f02
jjy <galid1@naver.com>
2019-04-27 00:12:53 +0900

    19.04.26
    bfs (3055)

--- 2.algorithm/DFS_BFS/escape.py
+++ 2.algorithm/DFS_BFS/escape.py
@@ -3,12 +3,14 @@
 import sys
 import collections
 
+
 # 고슴도치의 위치와 물의 위치에 모두 사용
 class Node:
-    def __init__(self, x, y, count = 0):
+    def __init__(self, x, y, count=0):
         self.x = x
         self.y = y
         self.count = count
+
 
 def escape(matrix):
     #     상 하 좌 우
@@ -36,16 +38,17 @@ def escape(matrix):
     # 고슴도치가 이동 불가능할 때 까지
     while s_queue_1:
         # 물처리 (현재 턴에 이동할 수 있는 물의 위치를 모두 이동하도록 해야함)
-        while w_queue_1 :
+        while w_queue_1:
             w_cur = w_queue_1.popleft()
             for k in range(4):
                 nx = w_cur.x + dx[k]
                 ny = w_cur.y + dy[k]
-                
+
                 # nx, ny 가 matrix 범위 안인 경우
                 if nx >= 0 and ny >= 0 and nx < len(matrix) and ny < len(matrix[0]):
                     # '.', 'S' 인 경우에만 물이 번지므로 이미 방문한 것인지 굳이 확인 안해도됨
-                    if (matrix[nx][ny] == '.' or matrix[nx][ny] == 'S') and matrix[nx][ny] != 'X' and matrix[nx][ny] != 'D':
+                    if (matrix[nx][ny] == '.' or matrix[nx][ny] == 'S') and matrix[nx][ny] != 'X' and matrix[nx][
+                        ny] != 'D':
                         w_queue_2.append(Node(nx, ny))
                         matrix[nx][ny] = '*'
 
@@ -53,7 +56,7 @@ def escape(matrix):
         w_queue_2 = collections.deque()
 
         # 고슴도치 이동처리
-        while s_queue_1 :
+        while s_queue_1:
             s_cur = s_queue_1.popleft()
             for g in range(4):
                 nx = s_cur.x + dx[g]
@@ -69,7 +72,7 @@ def escape(matrix):
                     # 방문처리 및 고슴도치 이동
                     if matrix[nx][ny] == '.' and matrix[nx][ny] != 'X' and visit[nx][ny] == -1:
                         s_queue_2.append(Node(nx, ny, s_cur.count + 1))
-                        matrix[nx][ny] = 'S' #이동 위치를 눈으로 보기위해 일부러 추가한 코드
+                        matrix[nx][ny] = 'S'  # 이동 위치를 눈으로 보기위해 일부러 추가한 코드
                         visit[nx][ny] = 1
 
         s_queue_1 = s_queue_2
@@ -77,6 +80,7 @@ def escape(matrix):
 
     # 도착 불가능
     print("KAKTUS")
+
 
 R, C = list(map(int, sys.stdin.readline().rstrip().split(" ")))
 matrix = []
