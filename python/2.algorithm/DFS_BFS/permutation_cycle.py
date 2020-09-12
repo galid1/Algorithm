3377ae1165af8078cad49e3d18f1ad51b9fe7f22
전준엽 <galid1@naver.com>
2020-09-12 12:36:36 +0900

    add 백준 10451 순열사이클

---
+++ python/2.algorithm/DFS_BFS/permutation_cycle.py
@@ -0,0 +1,41 @@
+# 백준 10451 순열사이클
+
+import sys
+
+def solve(path):
+    visit = [0 for k in range(1001)]
+    count = 0
+
+    for i in range(1, len(path)):
+        if not visit[i]:
+            count += 1
+            dfs(path, visit, i)
+
+    return count
+
+
+def dfs(path, visit, start):
+    stack = [start]
+    visit[start] = True
+
+    while stack:
+        cur = stack.pop()
+        next = path[cur]
+
+        if next == start:
+            return
+
+        visit[next] = True
+        stack.append(next)
+
+
+t_list = []
+t = int(sys.stdin.readline())
+for i in range(t):
+    t_num = int(sys.stdin.readline())
+    t_case = [0] + list(map(int, sys.stdin.readline().split(" ")))
+    t_list.append(t_case)
+
+for arr in t_list:
+    print(solve(arr))
+
