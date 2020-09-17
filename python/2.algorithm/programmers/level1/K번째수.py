b5a8112d35d56ed8a1e909e5db053473bf5956e8
jjy <galid1@naver.com>
2020-09-17 14:58:57 +0900

    add 프로그래머스 Level1 K번째 수

---
+++ python/2.algorithm/programmers/level1/K번째수.py
@@ -0,0 +1,21 @@
+# 프로그래머스 i번째 j번째까지 자르고 정렬한뒤 k번째 수 구하기
+
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        sub_solution(answer, array, command)
+
+    return answer
+
+
+def sub_solution(answer, array, command):
+    first = command[0] - 1
+    last = command[1]
+    sub_str = sorted(array[first:last])
+
+    k = command[2]
+    answer.append(sub_str[k-1])
+
+
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
