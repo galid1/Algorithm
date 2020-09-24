e75327e56df5df65a187e1652ff8b2a56c13eddb
jjy <galid1@naver.com>
2020-09-24 14:26:48 +0900

    add 프로그래머스 Level 2 주식가격

---
+++ python/2.algorithm/programmers/level2/주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = [0 for i in range(len(prices))]
+
+    for i in range(len(prices) - 1):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+
+    return answer
