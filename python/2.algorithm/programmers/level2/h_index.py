e4c0d034008aa7549bc1e7ed43a672b328e48192
jjy <galid1@naver.com>
2020-10-06 00:47:37 +0900

    add 프로그래머스 Level 2 h_index

---
+++ python/2.algorithm/programmers/level2/h_index.py
@@ -0,0 +1,21 @@
+def solution(citations):
+    answer = 0
+
+    citations.sort(reverse=True)
+
+    flag = False
+    for i in range(len(citations)):
+        if citations[i] < i+1:
+            answer = i
+            flag = True
+            break
+
+    if not flag:
+        answer = len(citations)
+
+    return answer
+
+solution([0])
+solution([2, 2])
+solution([4, 4, 4, 4])
+solution([3, 0, 6, 1, 5])
