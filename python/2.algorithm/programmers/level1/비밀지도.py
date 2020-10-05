cc2f0dfa1114a3bff892c53e8b06b56aaa5d77b1
jjy <galid1@naver.com>
2020-10-05 12:42:54 +0900

    mod 비밀지도 리팩토링

--- python/2.algorithm/programmers/level1/비밀지도.py
+++ python/2.algorithm/programmers/level1/비밀지도.py
@@ -1,39 +1,57 @@
 # 프로그래머스 Level 1 비밀지도
 
+# def solution(n, arr1, arr2):
+#     answer = []
+#
+#     cypher_arr = []
+#     # 두개의 지도로 해독
+#     for i in range(n):
+#         cypher = bin(arr1[i] | arr2[i])[2:]
+#         cypher_arr.append(cypher)
+#
+#     # 자리수 맞추기
+#     for i in range(n):
+#         cypher = cypher_arr[i]
+#
+#         changed = False
+#         less_len = abs(len(cypher) - n)
+#
+#         for j in range(less_len):
+#             cypher = '0' + cypher
+#             changed = True
+#
+#         if changed:
+#             cypher_arr[i] = cypher
+#
+#     for cypher in cypher_arr:
+#         cypher = list(cypher)
+#         plain = []
+#
+#         for c in cypher:
+#             if c == '1':
+#                 plain.append('#')
+#             else:
+#                 plain.append(' ')
+#
+#         answer.append(''.join(plain))
+#
+#     return answer
+
+
 def solution(n, arr1, arr2):
     answer = []
-
-    cypher_arr = []
-    # 두개의 지도로 해독
     for i in range(n):
-        cypher = bin(arr1[i] | arr2[i])[2:]
-        cypher_arr.append(cypher)
+        t_or = str(bin(arr1[i] | arr2[i]))[2:]
 
-    # 자리수 맞추기
-    for i in range(n):
-        cypher = cypher_arr[i]
+        if len(t_or) <= n:
+            need_cnt = n - len(t_or)
+            t_or = ('0' * need_cnt) + t_or
 
-        changed = False
-        less_len = abs(len(cypher) - n)
+        answer.append(t_or)
 
-        for j in range(less_len):
-            cypher = '0' + cypher
-            changed = True
-
-        if changed:
-            cypher_arr[i] = cypher
-
-    for cypher in cypher_arr:
-        cypher = list(cypher)
-        plain = []
-
-        for c in cypher:
-            if c == '1':
-                plain.append('#')
-            else:
-                plain.append(' ')
-
-        answer.append(''.join(plain))
+    for j in range(n):
+        answer[j] = answer[j].replace('1', '#')
+        answer[j] = answer[j].replace('0', ' ')
 
     return answer
 
