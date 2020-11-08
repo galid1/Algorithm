0e124d8a59353a7865733d876e57d6ef516bbf4b
jjy <galid1@naver.com>
2020-11-08 20:36:15 +0900

    mod 백준 한 줄로 서기 완성

--- python/2.algorithm/greedy/백준_한_줄로_서기.py
+++ python/2.algorithm/greedy/백준_한_줄로_서기.py
@@ -3,8 +3,27 @@ import sys
 
 def solution():
     global n, arr
+    answers = [0 for i in range(n)]
 
+    for i in range(n):
+        left = arr[i]
+        cur = i+1
+        idx = 0
 
+        while left > 0:
+            if answers[idx] == 0 or answers[idx] > cur:
+                idx += 1
+                left -= 1
+            elif answers[idx] < cur:
+                idx += 1
+
+        while answers[idx] != 0:
+            idx += 1
+
+        answers[idx] = cur
+
+    for a in answers:
+        print(a, end=' ')
 
 
 n = int(sys.stdin.readline())
