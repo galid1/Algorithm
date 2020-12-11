7425eb55632ffb0ab3f684681dc6252b45e77556
jjy <galid1@naver.com>
2020-12-11 14:38:18 +0900

    add 백준 감소하는 수

--- python/2.algorithm/brute_force/백준_감소하는_수.py
+++ python/2.algorithm/brute_force/백준_감소하는_수.py
@@ -1,56 +1,30 @@
 import sys
 
 
-def dfs(cur_num):
-    global n, answer, idx, printed, answers
+def dfs(cur_num, limit):
+    global answer, idx, n, answers
 
-    for i in range(10):
-        new_num = cur_num + str(i)
+    # 재귀 종료
+    if len(cur_num) == limit:
+        idx += 1
+        answers.append(cur_num)
+        # 정답이 존재
+        if idx == n:
+            print(cur_num)
+            sys.exit()
+        return
 
-        # 정답
-        if n == idx:
-            if not printed:
-                print(answer)
-            printed = True
-            return
-
-        if is_decreasing(new_num):
-            idx += 1
-            answer = new_num
-            answers.append(new_num)
-        else:
-            break
-
-    for j in range(0, 10):
-        init = ''
-        if cur_num:
-            init = cur_num[:-1]
-            new_num = init + str(int(cur_num[-1]) + j)
-        else:
-            new_num = 
-
-        if not is_decreasing(new_num):
-            continue
-        dfs(new_num)
+    if not cur_num:
+        for i in range(10):
+            dfs(str(i), limit)
+    else:
+        for j in range(int(cur_num[-1])):
+            dfs(cur_num + str(j), limit)
 
 
-def is_decreasing(num):
-    bef = num[0]
-
-    for i in range(1, len(num)):
-        if int(bef) <= int(num[i]):
-            return False
-
-        bef = num[i]
-
-    return True
-
-
-printed = False
-answers = []
 answer, idx = 0, -1
+answers = []
 n = int(sys.stdin.readline())
-dfs('')
-if not printed:
-    print(-1)
-print(answers)
+for i in range(1, 11):
+    dfs('', i)
+print(-1)
