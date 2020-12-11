9da2b1ff6d561d87ac0bd4428714fbdc683b624b
jjy <galid1@naver.com>
2020-12-11 11:01:29 +0900

    mod 백준 감소하는 수

--- python/2.algorithm/brute_force/백준_감소하는_수.py
+++ python/2.algorithm/brute_force/백준_감소하는_수.py
@@ -1,24 +1,37 @@
 import sys
 
 
-def solution():
-    global n
+def dfs(cur_num):
+    global n, answer, idx, printed, answers
 
-    answer, idx = 0, 0
+    for i in range(10):
+        new_num = cur_num + str(i)
 
-    for i in range(1, 1000000 + 1):
-        if idx == n:
+        # 정답
+        if n == idx:
+            if not printed:
+                print(answer)
+            printed = True
+            return
+
+        if is_decreasing(new_num):
+            idx += 1
+            answer = new_num
+            answers.append(new_num)
+        else:
             break
 
-        cur_num = str(i)
-        if is_decreasing(cur_num):
-            answer = i
-            idx += 1
+    for j in range(0, 10):
+        init = ''
+        if cur_num:
+            init = cur_num[:-1]
+            new_num = init + str(int(cur_num[-1]) + j)
+        else:
+            new_num = 
 
-    if idx != n:
-        print(-1)
-    else:
-        print(answer)
+        if not is_decreasing(new_num):
+            continue
+        dfs(new_num)
 
 
 def is_decreasing(num):
@@ -33,6 +46,11 @@ def is_decreasing(num):
     return True
 
 
-
+printed = False
+answers = []
+answer, idx = 0, -1
 n = int(sys.stdin.readline())
-solution()
+dfs('')
+if not printed:
+    print(-1)
+print(answers)
