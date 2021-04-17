bee45ab2120af417d53c2bc7fa71eb7cfe54f31b
전준엽 <galid1@naver.com>
2021-04-17 15:58:48 +0900

    add 백준 RGB 거리

--- python/2.algorithm/dynamic_programming/백준_RGB_거리.py
+++ python/2.algorithm/dynamic_programming/백준_RGB_거리.py
@@ -4,33 +4,22 @@ def solve():
     global n, rgbs
 
     # r ,g ,b 선
-    d = [0 for _ in range(n+1)]
-    selected = [0 for _ in range(n+1)]
-    d[0] = 0
+    d = [[1001 for _ in range(3)] for _ in range(n+1)]
+    d[0][0] = 0
+    d[0][1] = 0
+    d[0][2] = 0
 
-    # 미리 첫번째, 두번째 작은 수와, 그 idx를 구해 놓는다.
-
+    # d[i][0] == i번째 집을 R로 칠했을 때 가장 작은 값
     for i in range(1, n+1):
-        # 현재 rgb에서 제일 작은 값을 일단 찾는다.
-        min_rgb = 1001
-        min_idx = -1
-        for j in range(3):
-            min_rgb = min(min_rgb, rgbs[i][j])
-            min_idx = j
+        d[i][0] = rgbs[i][0] + min(d[i-1][1], d[i-1][2])
+        d[i][1] = rgbs[i][1] + min(d[i-1][0], d[i-1][2])
+        d[i][2] = rgbs[i][2] + min(d[i-1][0], d[i-1][1])
 
-        # 현재 선택한 rgb가 이전 rgb와 같으면, 이전 rgb를 모두 최신화 한 것과,
-        # 현재를 그냥 그다음 작은 값의 idx로 바꾼것 중 더 작은 것으로 선택
-        tmp_rgb_sum = min_rgb
-        if selected[i-1] == min_idx:
-            # i부터 0까지 최신화
-            for k in range(i-1, 0, -1):
-
-        if
-
+    print(min(d[n][0], d[n][1], d[n][2]))
 
 
 n = int(sys.stdin.readline().strip())
-rgbs = []
+rgbs = [0]
 for i in range(n):
     rgbs.append(list(map(int, sys.stdin.readline().strip().split(" "))))
 solve()
