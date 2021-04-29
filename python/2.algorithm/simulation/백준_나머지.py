0d65c4432008d1574f37f0017c3bc53f611dbb69
전준엽 <galid1@naver.com>
2021-04-29 13:59:32 +0900

    add 백준 나머지

---
+++ python/2.algorithm/simulation/백준_나머지.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def solve():
+    global nums
+
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C) * (B%C))%C)
+
+A, B, C = list(map(int, sys.stdin.readline().strip().split(" ")))
+solve()
+# (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C
