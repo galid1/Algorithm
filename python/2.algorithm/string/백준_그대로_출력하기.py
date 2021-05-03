b6bcf9b8fa1206adae6518dc48c7de2c2e77873b
전준엽 <galid1@naver.com>
2021-05-03 14:53:07 +0900

    add 백준 그대로 출력하기

---
+++ python/2.algorithm/string/백준_그대로_출력하기.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def solve():
+    global s
+    print(s)
+
+
+while True:
+    s = sys.stdin.readline().strip()
+    if not s:
+        break
+
+    solve()
+
+# Hello
+# Baekjoon
+# Online Judge
