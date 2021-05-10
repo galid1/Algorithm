bdbf4a503e717381378e0f2f16105fd5f2e9b62f
전준엽 <galid1@naver.com>
2021-05-10 14:13:35 +0900

    add 백준 별찍기 4

---
+++ python/2.algorithm/implementation/백준_별찍기_4.py
@@ -0,0 +1,20 @@
+import sys
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
+
+def solve():
+    global n
+
+    for i in range(n):
+        for j in range(i):
+            print(' ', end='')
+        for j in range(n-i):
+            print('*', end='')
+        print()
+
+n = int(sys.stdin.readline().strip())
+solve()
