e289c1354bf5bab20b338e0416e57036307a93c4
전준엽 <galid1@naver.com>
2021-05-24 16:06:28 +0900

    add 백준 자리배정

--- python/2.algorithm/implementation/백준_자리배정.py
+++ python/2.algorithm/implementation/백준_자리배정.py
@@ -1,38 +1,34 @@
 import sys
 
-
 def solve():
-    global r, c, target, ds
+    global n, m, target, ds
 
-    di = -1
-    num = 0
-    ex, ey = r, c
-    x, y = 0, 1
-
-    if target > r*c:
+    if target > n*m:
         return print(0)
 
+    ex, ey = n, m
+    x, y = 0, 1
+    di = -1
+    num = 0
     while True:
-        di = (di+1)%4
-
+        di = (di + 1)%4
         if di == 0 or di == 2:
             for i in range(ex):
+                num += 1
+                x += ds[di][0]
                 if num == target:
                     return print(y, x)
-                x += ds[di]
-                num += 1
             ey -= 1
         else:
             for i in range(ey):
+                num += 1
+                y += ds[di][1]
                 if num == target:
                     return print(y, x)
-                y += ds[di]
-                num += 1
             ex -= 1
 
 
-
-ds = [1, 1, -1, -1]
-c, r = map(int, sys.stdin.readline().strip().split(" "))
+ds = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+m, n = map(int, sys.stdin.readline().strip().split(" "))
 target = int(sys.stdin.readline().strip())
 solve()
