70305580f17046d99bc76d68fa71600583743d2a
전준엽 <galid1@naver.com>
2021-05-29 13:25:04 +0900

    add 백준 놀라운 문자열

--- python/2.algorithm/implementation/백준_놀라운_문자열.py
+++ python/2.algorithm/implementation/백준_놀라운_문자열.py
@@ -3,7 +3,32 @@ import sys
 
 def solve():
     global ss
-    print(ss)
+
+    for s in ss:
+        is_only = True
+        length = len(s)
+        # 건너띄기에 사용되는 변수
+        for i in range(1, length):
+            ds = set()
+            for idx, c in enumerate(s):
+                if idx+i >= length:
+                    break
+
+                pair = c + s[i+idx]
+                if pair in ds:
+                    is_only = False
+                    break
+                else:
+                    ds.add(pair)
+
+            if not is_only:
+                break
+
+        s = ''.join(s)
+        if is_only:
+            print(s, 'is surprising.')
+        else:
+            print(s, 'is NOT surprising.')
 
 
 ss = []
