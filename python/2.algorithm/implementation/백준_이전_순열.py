b2dffca5fdc3ff9a89b31f46ca1d076e07a3e0c4
전준엽 <galid1@naver.com>
2021-06-17 16:10:27 +0900

    add 백준 이전 순열

---
+++ python/2.algorithm/implementation/백준_이전_순열.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def solve():
+    global n, nums
+
+    # nums[k] > nums[k+1] 을 만족하는 가장 큰 k 구하기
+    k = 0
+    for i in range(n-1):
+        if nums[i] > nums[i+1]:
+            k = i
+
+    if k == 0:
+        return print(-1)
+
+    m = k+1
+    for i in range(k+1, n):
+        # k번째 수보다 작고 가장 큰 수 찾기
+        if nums[k] > nums[i] > nums[m]:
+            m = i
+
+    # 수 위치 변경
+    nums[k], nums[m] = nums[m], nums[k]
+
+    # m이후 수를 모두 역순으로
+    bef_arr = nums[:k+1] + sorted(nums[k+1:], reverse=True)
+    print(*bef_arr)
+
+
+
+n = int(sys.stdin.readline().strip())
+nums = list(map(int,sys.stdin.readline().strip().split(" ")))
+solve()
