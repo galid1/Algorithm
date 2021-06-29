76be9bfe59f61c8ec11aa286c968e6abfeadeca9
전준엽 <galid1@naver.com>
2021-06-29 14:32:45 +0900

    add 퀵정렬 가운데 피봇으로 정렬 추가

--- python/baekjoon/2.algorithm/sort/백준_퀵정렬.py
+++ python/baekjoon/2.algorithm/sort/백준_퀵정렬.py
@@ -1,36 +1,69 @@
 import sys
+sys.setrecursionlimit(1000000)
 
 
-def quick(nums, begin, end):
-    pivot = nums[begin]
-    L, R = begin+1, end
+# 첫번째 요소를 피벗으로 하여 정렬
+# def quick(begin, end):
+#     pivot = nums[begin]
+#     L, R = begin+1, end
+#
+#     while L <= R:
+#         while L <= end and nums[L] < pivot:
+#             L += 1
+#         while R > begin and nums[R] > pivot:
+#             R -= 1
+#
+#         if L > R:
+#             nums[begin], nums[R] = nums[R], nums[begin]
+#         else:
+#             nums[L], nums[R] = nums[R], nums[L]
+#             L, R = L+1, R-1
+#
+#     if R > begin:
+#         quick(begin, R)
+#     if L < end:
+#         quick(L, end)
+
+
+# 중앙값을 피벗으로하여 정렬
+def quick(begin, end):
+    global nums
+
+    pivot = nums[(begin+end)//2]
+    L, R = begin, end
 
     while L <= R:
-        while L <= end and nums[L] < pivot:
+        while nums[L] < pivot:
             L += 1
-        while R > begin and nums[R] > pivot:
+        while nums[R] > pivot:
             R -= 1
 
-        if L > R:
-            nums[begin], nums[R] = nums[R], nums[begin]
-        else:
+        if L <= R:
             nums[L], nums[R] = nums[R], nums[L]
             L, R = L+1, R-1
 
-    if R > begin:
-        quick(nums, begin, R)
     if L < end:
-        quick(nums, L, end)
-#
-#
-# n = int(sys.stdin.readline().strip())
-# nums = []
-# for i in range(n):
-#     nums.append(int(sys.stdin.readline().strip()))
-# quick(0, n-1)
-#
-# for num in nums:
-#     print(num)
+        quick(L, end)
+    if R > begin:
+        quick(begin, R)
+
+
+n = int(sys.stdin.readline().strip())
+nums = []
+for i in range(n):
+    nums.append(int(sys.stdin.readline().strip()))
+quick(0, n-1)
+
+for num in nums:
+    print(num)
+
+# 5
+# 3
+# 7
+# 1
+# 4
+# 2
+
 # 5
 # 5
 # 4
