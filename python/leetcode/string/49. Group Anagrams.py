2723f1e76d4aa6b07a67fd94dd067b5c34eaab70
전준엽 <galid1@naver.com>
2021-06-30 13:27:44 +0900

    leetcode 49. Group Anagrams

---
+++ python/leetcode/string/49. Group Anagrams.py
@@ -0,0 +1,17 @@
+def groupAnagrams(strs):
+    ans = {}
+    for s in strs:
+        sorted_s = sorted(list(s))
+        sorted_s = ''.join(sorted_s)
+
+        if sorted_s in ans.keys():
+            ans[sorted_s].append(s)
+        else:
+            ans[sorted_s] = [s]
+
+    return list(ans.values())
+
+
+strs = ["eat","tea","tan","ate","nat","bat"]
+groupAnagrams(strs)
+# [["bat"],["nat","tan"],["ate","eat","tea"]]
