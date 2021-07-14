6d40c95caa9af79d421fbee152553919694d2e91
전준엽 <galid1@naver.com>
2021-07-14 12:45:16 +0900

    leetcode 외부 반복 없이 조합 77. Combinations

--- python/leetcode/dfs/77. Combinations.py
+++ python/leetcode/dfs/77. Combinations.py
@@ -1,18 +1,33 @@
+# def combine(n: int, k: int):
+#     def dfs(nums, cur_num: list, cnt):
+#         if cnt == k:
+#             ans.append(nums.copy())
+#             return
+#
+#         for i in range(cur_num+1, n+1):
+#             nums.append(i)
+#             dfs(nums, i, cnt+1)
+#             nums.pop()
+#
+#     ans = []
+#     for i in range(1, n+1):
+#         dfs([i], i, 1)
+#     return ans
+
+
 def combine(n: int, k: int):
-    def dfs(nums, cur_num: list, cnt):
+    def dfs(cur_num, nums:list, cnt):
         if cnt == k:
             ans.append(nums.copy())
             return
 
-        for i in range(cur_num+1, n+1):
+        for i in range(cur_num, n+1):
             nums.append(i)
-            dfs(nums, i, cnt+1)
+            dfs(i+1, nums, cnt+1)
             nums.pop()
 
     ans = []
-    for i in range(1, n+1):
-        dfs([i], i, 1)
+    dfs(1, [], 0)
     return ans
 
-
-print(combine(1, 1))
+print(combine(4, 2))
