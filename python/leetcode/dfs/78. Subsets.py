b1f2430eddd0cea99aa22c8320e371eecd02087a
전준엽 <galid1@naver.com>
2021-07-14 13:27:30 +0900

    leetcode 78. Subsets 최적화

--- python/leetcode/dfs/78. Subsets.py
+++ python/leetcode/dfs/78. Subsets.py
@@ -1,18 +1,15 @@
 def subsets(nums):
-    def dfs(k, cur_nums:list, cur_idx, cnt):
-        if cnt == k:
-            ans.append(cur_nums.copy())
-            return
+    def dfs(cur_nums:list, cur_idx, cnt):
+        ans.append(cur_nums.copy())
 
         for i in range(cur_idx, length):
             cur_nums.append(nums[i])
-            dfs(k, cur_nums, i+1, cnt+1)
+            dfs(cur_nums, i+1, cnt+1)
             cur_nums.pop()
 
     ans = []
     length = len(nums)
-    for k in range(0, length+1):
-        dfs(k, [], 0, 0)
+    dfs([], 0, 0)
 
     return ans
 
