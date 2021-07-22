c4daf14e3ef3225bd57662d33c080c5cbbd9c68b
전준엽 <galid1@naver.com>
2021-07-22 11:55:53 +0900

    leetcode 215. Kth Largest Element in an Array(Heap 풀이 추가 성능향상)

--- python/leetcode/datastructure/array/215. Kth Largest Element in an Array.py
+++ python/leetcode/datastructure/array/215. Kth Largest Element in an Array.py
@@ -1,12 +1,29 @@
+# sort 이용
+# class Solution:
+#     def findKthLargest(self, nums, k: int) -> int:
+#         nums.sort(reverse=True)
+#         return nums[k-1]
+
+
+# heal이용
+import heapq
+
 class Solution:
     def findKthLargest(self, nums, k: int) -> int:
-        nums.sort(reverse=True)
-        return nums[k-1]
+        heap = []
+        for n in nums:
+            heapq.heappush(heap, -n)
+
+        while k > 1:
+            k -= 1
+            heapq.heappop(heap)
+
+        return -heapq.heappop(heap)
 
 
-# nums =  [3,2,1,5,6,4]
-# k = 2
-nums = [3,2,3,1,2,4,5,5,6]
-k = 4
+nums =  [3,2,1,5,6,4]
+k = 2
+# nums = [3,2,3,1,2,4,5,5,6]
+# k = 4
 s = Solution()
 print(s.findKthLargest(nums, k))
