f50a0b9be41bc70caa9494e8360f59a2ce7ea2f7
전준엽 <galid1@naver.com>
2021-08-01 09:58:57 +0900

    fix 1205

--- python/baekjoon/2.algorithm/implementation/1205.등수_구하기.py
+++ python/baekjoon/2.algorithm/implementation/1205.등수_구하기.py
@@ -2,21 +2,29 @@ import sys
 
 
 def solve(n, grade, p, nums):
-    idx, rank = -1, 0
-
-    while idx + 1 < n:
-        idx, rank = idx+1, rank+1
-
-        if grade >= nums[idx]:
-            while idx < n and grade == nums[idx]:
-                idx += 1
-                if idx >= p:
-                    return print(-1)
-
+    # 현재 점수와 grade 같을 때 까지 rank, idx를 계속 올린다.
+    # idx가 p와 같다면 -1을 리턴
+    # 현재 점수와 grade가 같다면, 같지 않을때까지 idx를 증가 p와 같다면 -1 리턴
+    idx, rank = 0, 1
+    while idx < n:
+        if nums[idx] < grade:
             return print(rank)
-    if idx >= p:
+
+        flag = False
+        while idx < n and grade == nums[idx]:
+            flag = True
+            idx, rank = idx+1, rank+1
+            if idx + 1 > p:
+                return print(-1)
+
+        if flag:
+            return print(rank-1)
+
+        idx, rank = idx + 1, rank + 1
+
+    if idx+1 > p:
         return print(-1)
-    print(rank)
+    print(rank-1)
 
 
 n, grade, p = map(int, sys.stdin.readline().strip().split(" "))
@@ -27,6 +35,8 @@ if not nums_s:
 nums = list(map(int, nums_s.split(" ")))
 solve(n, grade, p, nums)
 
+# 3 5 3
+# 30 20 10
 
 # 4 80 4
 # 100 90 90 80
@@ -36,5 +46,17 @@ solve(n, grade, p, nums)
 # 6 3 5
 # 10 10 10 10 5 4
 
-# 4 20 5
+# 4 20 4
 # 10 10 10 10
+
+# 4 10 4
+# 10 10 10 10
+
+# 3 10 3
+# 20 10 10
+# -1!
+
+
+# 5 2 5
+# 6 5 4 3 1
+# 5!
