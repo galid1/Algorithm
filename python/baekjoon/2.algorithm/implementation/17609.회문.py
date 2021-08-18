b6fd4dea9b911f1b655809152ccccbdef06645c1
전준엽 <galid1@naver.com>
2021-08-18 21:59:51 +0900

    백준 17609. 회문(반복문)

--- python/baekjoon/2.algorithm/implementation/17609.회문.py
+++ python/baekjoon/2.algorithm/implementation/17609.회문.py
@@ -1,28 +1,60 @@
-import sys
-sys.setrecursionlimit(100000)
+# 재귀 풀이
+# import sys
+# sys.setrecursionlimit(100000)
+#
+# def solve(n, ss):
+#     for s in ss:
+#         print(decide_palindrome(s, 0, len(s)-1, False))
+#
+#
+# def decide_palindrome(s, L, R, skip=False):
+#     if L >= R:
+#         if skip:
+#             return 1
+#         else:
+#             return 0
+#
+#     if s[L] == s[R]:
+#         return decide_palindrome(s, L+1, R-1, skip)
+#     else:
+#         if not skip:
+#             a = decide_palindrome(s, L+1, R, True)
+#             b = decide_palindrome(s, L, R-1, True)
+#             return min(a, b)
+#         else:
+#             return 2
+#
+#
 
+import sys
 
 def solve(n, ss):
     for s in ss:
-        print(decide_palindrome(s, 0, len(s)-1, False))
+        L, R = is_palindrome(s, 0, len(s) - 1)
 
-
-def decide_palindrome(s, L, R, skip=False):
-    if L >= R:
-        if skip:
-            return 1
+        if L >= R:
+            print(0)
         else:
-            return 0
+            L1, R1 = is_palindrome(s, L+1, R)
+            if L1 >= R1:
+                print(1)
+                continue
 
-    if s[L] == s[R]:
-        return decide_palindrome(s, L+1, R-1, skip)
-    else:
-        if not skip:
-            a = decide_palindrome(s, L+1, R, True)
-            b = decide_palindrome(s, L, R-1, True)
-            return min(a, b)
+            L2, R2 = is_palindrome(s, L, R-1)
+            if L2 >= R2:
+                print(1)
+                continue
+
+            print(2)
+
+
+def is_palindrome(s, L, R):
+    while L <= R:
+        if s[L] == s[R]:
+            L, R = L + 1, R - 1
         else:
-            return 2
+            break
+    return L, R
 
 
 n = int(sys.stdin.readline().strip())
@@ -33,25 +65,5 @@ for _ in range(n):
 solve(n, ss)
 
 
-# 5
-# zaa
-# zaba
-# azba
-# aba
-# abab
-
-
-# 3
-# abba
-# abcba
-# abbba
-
-
-# 5
-# abcbba
-# adbbdd
-# ddddd
-# d
-# dd
 
 
