ea034664923bead463a0fc44213134afdd765433
전준엽 <galid1@naver.com>
2021-08-19 16:05:12 +0900

    백준 10800.컬러볼 (투포인터 풀이 추가)

--- python/baekjoon/2.algorithm/implementation/10800.컬러볼.py
+++ python/baekjoon/2.algorithm/implementation/10800.컬러볼.py
@@ -2,51 +2,78 @@ import sys
 from collections import defaultdict
 
 
-def solve(infos):
-    acc_colors = defaultdict(int)
-    sums = 0
+# def solve(infos):
+#     acc_colors = defaultdict(int)
+#     sums = 0
+#
+#     append_idx(infos)
+#     infos = sort_by_color_and_size(infos)
+#
+#     total_sum = 0
+#     # color별 누적
+#     color_acc = defaultdict(int)
+#
+#     # 같은 크기 누적
+#     cur_size = -1
+#     cur_same_size_sum = 0
+#     same_color_size = defaultdict(int)
+#
+#     # ans 구하기
+#     ans = defaultdict(int)
+#     for color, size, idx in infos:
+#         # 현재 같은 크기 정보 갱신
+#         if size != cur_size:
+#             cur_size = size
+#             same_color_size = defaultdict(int)
+#             same_color_size[color] += size
+#             cur_same_size_sum = size
+#         else:
+#             same_color_size[color] += size
+#             cur_same_size_sum += size
+#
+#         ans[idx] = total_sum - color_acc[color] - (cur_same_size_sum - same_color_size[color])
+#
+#         # 누적
+#         total_sum += size
+#         color_acc[color] += size
+#
+#
+#     ans = sorted(ans.items(), key=lambda item:item[0])
+#     # 임시 출력
+#     for an in ans:
+#         print(an[1])
 
+
+# 2 포인터 풀이
+def solve(infos):
     append_idx(infos)
     infos = sort_by_color_and_size(infos)
 
-    total_sum = 0
-    # color별 누적
-    color_acc = defaultdict(int)
-
-    # 같은 크기 누적
-    cur_size = -1
-    cur_same_size_sum = 0
-    same_color_size = defaultdict(int)
-
-    # ans 구하기
     ans = defaultdict(int)
-    for color, size, idx in infos:
-        # 현재 같은 크기 정보 갱신
-        if size != cur_size:
-            cur_size = size
-            same_color_size = defaultdict(int)
-            same_color_size[color] += size
-            cur_same_size_sum = size
-        else:
-            same_color_size[color] += size
-            cur_same_size_sum += size
+    acc = 0
+    color_acc = defaultdict(int)
+    j = 0
+    for i in range(len(infos)):
+        cur_color, cur_size, idx = infos[i]
 
-        ans[idx] = total_sum - color_acc[color] - (cur_same_size_sum - same_color_size[color])
+        compared_color, compared_size, compared_idx = infos[j]
 
-        # 누적
-        total_sum += size
-        color_acc[color] += size
+        while cur_size > compared_size:
+            acc += compared_size
+            color_acc[compared_color] += compared_size
+            j += 1
+            compared_color, compared_size, compared_idx = infos[j]
 
+        ans[idx] = acc - color_acc[cur_color]
 
     ans = sorted(ans.items(), key=lambda item:item[0])
-    # 임시 출력
     for an in ans:
         print(an[1])
 
 
 def append_idx(infos):
     for idx, info in enumerate(infos):
-        info.append(idx+1)
+        info.append(idx)
 
 
 def sort_by_color_and_size(infos):
