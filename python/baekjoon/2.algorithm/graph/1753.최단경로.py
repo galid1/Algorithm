984a2eb11a2c0e0458ace5c227cc6be9428da322
전준엽 <galid1@naver.com>
2021-10-09 17:16:50 +0900

    백준 1753. 최단경로 (거의 2배 빨라짐)

--- python/baekjoon/2.algorithm/graph/1753.최단경로.py
+++ python/baekjoon/2.algorithm/graph/1753.최단경로.py
@@ -1,45 +1,103 @@
+# import sys, heapq
+# from collections import defaultdict
+#
+#
+# def solve():
+#     global v, e, i, g
+#
+#     queue = [[0, i]]
+#     min_distances = defaultdict(int)
+#
+#     while queue:
+#         cur_w, cur_node = heapq.heappop(queue)
+#
+#         # 방문한 경우 다음으로
+#         if cur_node in min_distances:
+#             continue
+#
+#         min_distances[cur_node] = cur_w
+#
+#         for next_node, w in g[cur_node].items():
+#             heapq.heappush(queue, [(cur_w + w), next_node])
+#
+#     for j in range(1, v + 1):
+#         if j not in min_distances.keys():
+#             print("INF")
+#         else:
+#             print(min_distances[j])
+#
+#
+# v, e = map(int, sys.stdin.readline().strip().split(" "))
+# i = int(sys.stdin.readline().strip())
+# g = defaultdict(dict)
+# g[i][i] = 0
+# for _ in range(e):
+#     f, t, w = map(int, sys.stdin.readline().strip().split(" "))
+#
+#     if t not in g[f].keys():
+#         g[f][t] = w
+#     else:
+#         g[f][t] = min(g[f][t], w)
+#
+# solve()
+
+
 import sys, heapq
 from collections import defaultdict
 
 
 def solve():
-    global v, e, i, g
+    global v, e, s, g
 
-    queue = [[0, i]]
-    min_distances = defaultdict(int)
+    INF = sys.maxsize
+    visited = [False for _ in range(v)]
+    visited[s] = True
 
-    while queue:
-        cur_w, cur_node = heapq.heappop(queue)
+    dis = [INF for _ in range(v)]
+    dis[s] = 0
 
-        # 방문한 경우 다음으로
-        if cur_node in min_distances:
+    hq = []
+    for dest, w in g[s].items():
+        heapq.heappush(hq, (w, dest))
+
+    while hq:
+        w, dest = heapq.heappop(hq)
+
+        if visited[dest]:
             continue
 
-        min_distances[cur_node] = cur_w
+        visited[dest] = True
+        dis[dest] = w
 
-        for next_node, w in g[cur_node].items():
-            heapq.heappush(queue, [(cur_w + w), next_node])
+        for n_dest, nw in g[dest].items():
+            if visited[n_dest]:
+                continue
 
-    for j in range(1, v + 1):
-        if j not in min_distances.keys():
-            print("INF")
+            heapq.heappush(hq, (w + nw, n_dest))
+
+    for d in dis:
+        if d == INF:
+            print('INF')
         else:
-            print(min_distances[j])
+            print(d)
 
 
-v, e = map(int, sys.stdin.readline().strip().split(" "))
-i = int(sys.stdin.readline().strip())
+
+v, e = map(int, sys.stdin.readline().strip().split(' '))
+s = int(sys.stdin.readline().strip()) - 1
 g = defaultdict(dict)
-g[i][i] = 0
+
 for _ in range(e):
     f, t, w = map(int, sys.stdin.readline().strip().split(" "))
+    f, t = f-1, t-1
 
-    if t not in g[f].keys():
-        g[f][t] = w
-    else:
+    if t in g[f].keys():
         g[f][t] = min(g[f][t], w)
+    else:
+        g[f][t] = w
 
 solve()
+
 
 # 5 9
 # 1
