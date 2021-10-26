d85b1ae096fcb6352ba1e953528229208d1650eb
전준엽 <galid1@naver.com>
2021-10-26 16:18:16 +0900

    백준 1747.소수&팰린드롬 에라토테네스체 풀이 추가

--- python/baekjoon/2.algorithm/brute_force/1747.소수&팰린드롬.py
+++ python/baekjoon/2.algorithm/brute_force/1747.소수&팰린드롬.py
@@ -1,32 +1,77 @@
-import sys, math
+# import sys, math
+#
+#
+# def solve():
+#     global n
+#
+#     while True:
+#         if find(n):
+#             break
+#         n += 1
+#
+#     print(n)
+#
+#
+# def find(num):
+#     num_str = str(num)
+#
+#     if num < 2:
+#         return False
+#
+#     for i in range(len(num_str)//2):
+#         if num_str[i] != num_str[len(num_str)-1-i]:
+#             return False
+#
+#     for i in range(2, int(math.sqrt(num))+1):
+#         if num%i == 0:
+#             return False
+#
+#     return True
+#
+#
+# n = int(sys.stdin.readline().strip())
+# solve()
+
+import sys
 
 
 def solve():
     global n
 
-    while True:
-        if find(n):
+    arr = init()
+    ans = -1
+
+    for num in range(n, len(arr)):
+        if not arr[num]:
+            continue
+
+        if palindrome(num):
+            ans = num
             break
-        n += 1
 
-    print(n)
+    print(ans)
 
 
-def find(num):
-    num_str = str(num)
+def palindrome(num):
+    str_num = str(num)
 
-    if num < 2:
-        return False
-
-    for i in range(len(num_str)//2):
-        if num_str[i] != num_str[len(num_str)-1-i]:
+    for i in range(len(str_num)//2):
+        if str_num[i] != str_num[len(str_num)-1-i]:
             return False
-
-    for i in range(2, int(math.sqrt(num))+1):
-        if num%i == 0:
-            return False
-
     return True
+
+
+def init():
+    arr = [True for _ in range(1004000)]
+    arr[0] = False
+    arr[1] = False
+
+    for i in range(2, 1000400):
+        if arr[i]:
+            for j in range(i*2, len(arr), i):
+                arr[j] = False
+
+    return arr
 
 
 n = int(sys.stdin.readline().strip())
