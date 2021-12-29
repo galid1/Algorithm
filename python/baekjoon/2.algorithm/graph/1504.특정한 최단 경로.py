2d661ea0892f4a21a8b57095076ae00990706815
전준엽 <galid1@naver.com>
2021-12-29 13:52:53 +0900

    백준 1504. 특정한 최단 경로

--- python/baekjoon/2.algorithm/graph/1504.특정한 최단 경로.py
+++ python/baekjoon/2.algorithm/graph/1504.특정한 최단 경로.py
@@ -1,23 +1,100 @@
-import sys
+import sys, heapq
+from collections import defaultdict
 
 
 def solve():
-    global n, e, es, v1, v2
+    global n, e, es, v1, v2, INF
 
-    for line in es:
-        print(line)
+    # a -> v1 -> v2 -> n
+    # a -> v2 -> v1 -> n
+    path1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, n)
+    path2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, n)
+    ans = min(path1, path2)
+
+    if ans >= INF:
+        print(-1)
+    else:
+        print(ans)
 
 
-INF = 1001
+def dijkstra(_from, _to):
+    global n, g, INF
+
+    dis_arr = [INF for _ in range(n+1)]
+    dis_arr[_from] = 0
+    visited = [False for _ in range(n+1)]
+    visited[_from] = True
+
+    q = []
+    for dest, w in g[_from].items():
+        heapq.heappush(q, (w, dest))
+
+    while q:
+        w, dest = heapq.heappop(q)
+
+        if visited[dest]:
+            continue
+
+        visited[dest] = True
+        dis_arr[dest] = w
+
+        for n_dest, n_w in g[dest].items():
+            if visited[n_dest]:
+                continue
+
+            heapq.heappush(q, (w + n_w, n_dest))
+
+    return dis_arr[_to]
+
+
+INF = sys.maxsize
 n, e = map(int, sys.stdin.readline().strip().split(" "))
-es = [[INF if i != j else 0 for i in range(n)] for j in range(n)]
+g = defaultdict(dict)
 
 for _ in range(e):
-    a, b, c = map(int, sys.stdin.readline().strip().split(" "))
-    a, b = a-1, b-1
-    es[a][b] = min(es[a][b], c)
-    es[b][a] = min(es[b][a], c)
+    f, t, c = map(int, sys.stdin.readline().strip().split(" "))
+    if t in g[f].keys():
+        g[f][t] = min(g[f][t], c)
+    else:
+        g[f][t] = c
+
+for i in range(1, n + 1):
+    g[i][i] = 0
 
 v1, v2 = map(int, sys.stdin.readline().strip().split(" "))
 
 solve()
+
+
+
+
+
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+n, e = map(int, input().split())
+s = [[] for i in range(n + 1)]
+inf = sys.maxsize
+for i in range(e):
+    a, b, c = map(int, input().split())
+    s[a].append([b, c])
+    s[b].append([a, c])
+v1, v2 = map(int, input().split())
+def dijkstra(start):
+    dp = [inf for i in range(n + 1)]
+    dp[start] = 0
+    heap = []
+    heappush(heap, [0, start])
+    while heap:
+        w, c = heappop(heap)
+        for n_n, n_w in s[c]:
+            wei = n_w + w
+            if dp[n_n] > wei:
+                dp[n_n] = wei
+                heappush(heap, [wei, n_n])
+    return dp
+one = dijkstra(1)
+v1_ = dijkstra(v1)
+v2_ = dijkstra(v2)
+cnt = min(one[v1] + v1_[v2] + v2_[n], one[v2] + v2_[v1] + v1_[n])
+print(cnt if cnt < inf else -1)
--- python/baekjoon/2.algorithm/graph/1753.최단경로.py
+++ python/baekjoon/2.algorithm/graph/1753.최단경로.py
@@ -42,62 +42,62 @@
 # solve()
 
 
-import sys, heapq
-from collections import defaultdict
-
-
-def solve():
-    global v, e, s, g
-
-    INF = sys.maxsize
-    visited = [False for _ in range(v)]
-    visited[s] = True
-
-    dis = [INF for _ in range(v)]
-    dis[s] = 0
-
-    hq = []
-    for dest, w in g[s].items():
-        heapq.heappush(hq, (w, dest))
-
-    while hq:
-        w, dest = heapq.heappop(hq)
-
-        if visited[dest]:
-            continue
-
-        visited[dest] = True
-        dis[dest] = w
-
-        for n_dest, nw in g[dest].items():
-            if visited[n_dest]:
-                continue
-
-            heapq.heappush(hq, (w + nw, n_dest))
-
-    for d in dis:
-        if d == INF:
-            print('INF')
-        else:
-            print(d)
-
-
-
-v, e = map(int, sys.stdin.readline().strip().split(' '))
-s = int(sys.stdin.readline().strip()) - 1
-g = defaultdict(dict)
-
-for _ in range(e):
-    f, t, w = map(int, sys.stdin.readline().strip().split(" "))
-    f, t = f-1, t-1
-
-    if t in g[f].keys():
-        g[f][t] = min(g[f][t], w)
-    else:
-        g[f][t] = w
-
-solve()
-
+# import sys, heapq
+# from collections import defaultdict
+#
+#
+# def solve():
+#     global v, e, s, g
+#
+#     INF = sys.maxsize
+#     visited = [False for _ in range(v)]
+#     visited[s] = True
+#
+#     dis = [INF for _ in range(v)]
+#     dis[s] = 0
+#
+#     hq = []
+#     for dest, w in g[s].items():
+#         heapq.heappush(hq, (w, dest))
+#
+#     while hq:
+#         w, dest = heapq.heappop(hq)
+#
+#         if visited[dest]:
+#             continue
+#
+#         visited[dest] = True
+#         dis[dest] = w
+#
+#         for n_dest, nw in g[dest].items():
+#             if visited[n_dest]:
+#                 continue
+#
+#             heapq.heappush(hq, (w + nw, n_dest))
+#
+#     for d in dis:
+#         if d == INF:
+#             print('INF')
+#         else:
+#             print(d)
+#
+#
+#
+# v, e = map(int, sys.stdin.readline().strip().split(' '))
+# s = int(sys.stdin.readline().strip()) - 1
+# g = defaultdict(dict)
+#
+# for _ in range(e):
+#     f, t, w = map(int, sys.stdin.readline().strip().split(" "))
+#     f, t = f-1, t-1
+#
+#     if t in g[f].keys():
+#         g[f][t] = min(g[f][t], w)
+#     else:
+#         g[f][t] = w
+#
+# solve()
+#
 
 # 5 9
 # 1
@@ -110,3 +110,70 @@ solve()
 # 1 4 1
 # 3 5 1
 # 4 3 1
+
+
+import sys, heapq
+from collections import defaultdict
+
+
+def dijkstra(start):
+    global g, v, INF
+
+    visited = [False for _ in range(v + 1)]
+    visited[start] = True
+
+    dis_arr = [INF for _ in range(v + 1)]
+    for to, w in g[start].items():
+        dis_arr[to] = w
+
+    q = []
+    for idx, w in enumerate(dis_arr):
+        if idx == 0 or w == INF:
+            continue
+
+        if visited[idx]:
+            continue
+
+        heapq.heappush(q, (w, idx))
+
+    while q:
+        w, to = heapq.heappop(q)
+
+        if visited[to]:
+            continue
+
+        visited[to] = True
+
+        for next, next_w in g[to].items():
+            if next_w == INF:
+                continue
+
+            trans_cost = w + next_w
+            if dis_arr[next] > trans_cost:
+                dis_arr[next] = trans_cost
+                heapq.heappush(q, (trans_cost, next))
+
+    return dis_arr
+
+
+INF = sys.maxsize
+v, e = map(int, sys.stdin.readline().strip().split(" "))
+k = int(sys.stdin.readline().strip())
+g = defaultdict(dict)
+for _ in range(e):
+    f, t, w = map(int, sys.stdin.readline().strip().split(" "))
+
+    if t in g[f].keys():
+        g[f][t] = min(g[f][t], w)
+    else:
+        g[f][t] = w
+
+for i in range(1, v+1):
+    g[i][i] = 0
+
+
+for w in dijkstra(k)[1:]:
+    if w == INF:
+        print("INF")
+    else:
+        print(w)
